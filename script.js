b3d1fb85a59f7ff09b6604866d29aa9c215a8af9
ManeeshWije <m.mwije1@gmail.com>
2021-09-04 22:00:18 -0400

    basically done i think

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+const changeBody = (pageName) => {
+  let img = document.createElement("img");
+  img.style.cssText = "height: 100vh;width:100%;";
+  img.src = chrome.runtime.getURL("dad.jpg");
+  document.body.innerHTML = "";
+  document.body.appendChild(img);
+
+  let h1 = document.createElement("H1");
+  h1.style.cssText =
+    "text-align:right;position:absolute;top:50px;left:50%;font-size:50px;color:white;";
+  let text = document.createTextNode("bro pls.");
+  h1.appendChild(text);
+
+  document.body.appendChild(h1);
+};
 switch (window.location.hostname) {
   case "www.youtube.com":
-    alert("bro pls");
+    changeBody();
     break;
 }
